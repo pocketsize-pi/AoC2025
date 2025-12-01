4df47c1e1b04081c98be75565cd3ab4ef4dbb7c5
pocketsize-pi <35309820+pocketsize-pi@users.noreply.github.com>
2025-12-01 13:57:39 +0000

    Day 01 part 2
    I tried clever before but it didn't work, so we did brute force in the end.

--- src/day01.rs
+++ src/day01.rs
@@ -13,7 +13,8 @@ pub fn day01(input_type: InputType, manual_name: &str) -> Result<(), Box<dyn std
         // println!("{:?}", row);
         let dir = row[0].chars().nth(0).unwrap();
         // println!("{:?}", dir);
-        let number : Modulo = (str_to_i32(&row[0][1..])).to_modulo(CIRCLE_LIMIT);
+        // let number : Modulo = (str_to_i32(&row[0][1..])).to_modulo(CIRCLE_LIMIT);
+        let number : i32 = str_to_i32(&row[0][1..]);
         // println!("{number}");
         list_movements.push((dir, number));
     }
@@ -25,13 +26,14 @@ pub fn day01(input_type: InputType, manual_name: &str) -> Result<(), Box<dyn std
     let mut dial = modulo!(50,CIRCLE_LIMIT); // it helps so much if we get the right starting position
     let mut num_zero = 0;
 
-    for (dir, number) in list_movements {
+    for (dir, number) in &list_movements {
 
-        if dir == 'R' {
-            dial = dial + number;
+
+        if *dir == 'R' {
+            dial = dial + modulo!(number, CIRCLE_LIMIT);
         }
         else {
-            dial = dial - number;
+            dial = dial - modulo!(number, CIRCLE_LIMIT);
         }
         // debug print
         // println!("dir: {}, offset: {}, final: {}", dir, number, dial);
@@ -40,10 +42,46 @@ pub fn day01(input_type: InputType, manual_name: &str) -> Result<(), Box<dyn std
         if dial == modulo!(0,CIRCLE_LIMIT) {
             num_zero += 1;
         }
+
     }
 
     println!("We go through zero {} times", num_zero);
     // 1172
+
+    // for part 2, we have removed the built-in modulo so that we have access to the "raw" number
+    // we build the number non-modular
+    // then we brute force, 'cause nice didn't work!
+
+    dial = modulo!(50,CIRCLE_LIMIT);
+    num_zero = 0;
+    for (dir, number) in &list_movements {
+        // move full blast
+        if *dir == 'R' {
+            for _i in 0..*number {
+                dial = dial + modulo!(1,CIRCLE_LIMIT);
+                if dial == modulo!(0,CIRCLE_LIMIT) {
+                    num_zero += 1;
+                }
+            }
+        }
+        else {
+            for _i in 0..*number {
+                dial = dial - modulo!(1,CIRCLE_LIMIT);
+                if dial == modulo!(0,CIRCLE_LIMIT) {
+                    num_zero += 1;
+                }
+            }
+        }
+
+        // debug
+        // println!("dir: {}, offset: {}, final: {}", dir, number, big_dial);
+
+    }
+
+    println!("We go through zero {} times", num_zero);
+    // 7260 too high
+    // 6088 too low! as is 6129
+    // 6932 Hurrah! Brute force works!
 
     Ok(())
 }
