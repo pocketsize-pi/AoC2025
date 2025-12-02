f25ac5ab6c9d0818709121a9b84291ad27e8794c
pocketsize-pi <35309820+pocketsize-pi@users.noreply.github.com>
2025-12-02 14:51:34 +0000

    Day 2 part 2

--- src/day02.rs
+++ src/day02.rs
@@ -1,3 +1,4 @@
+use divisors::get_divisors;
 use aoc_2025::*;
 
 pub fn day02(input_type: InputType, manual_name: &str) -> Result<(), Box<dyn std::error::Error>> {
@@ -21,7 +22,7 @@ pub fn day02(input_type: InputType, manual_name: &str) -> Result<(), Box<dyn std
     let mut num_invalid = 0;
     let mut invalid_sum = 0;
 
-    for pair in ranges_list {
+    for pair in &ranges_list {
         for id in pair.0..pair.1+1 {
             // convert number to string
             let id_str = id.to_string();
@@ -41,6 +42,41 @@ pub fn day02(input_type: InputType, manual_name: &str) -> Result<(), Box<dyn std
 
     println!("There are {} invalid ids, for a sum of {}", num_invalid, invalid_sum);
     // 30323879646 yay
+
+    // ok, let's still brute force part 2
+    num_invalid = 0;
+    invalid_sum = 0;
+
+    for pair in &ranges_list {
+        for id   in pair.0..pair.1+1 {
+            // convert number to string
+            let id_str = id.to_string();
+            // get divisors - I could do this manually, but there is a crate for it
+            let mut divs = get_divisors(id_str.len());
+            // note that this does not give us one! so we need to make that ourselves
+            divs.insert(0,1);
+            // println!("{id} -----");
+
+            for div in divs {
+                // println!(" div {div}");
+                let min_sxn = id_str[..div].to_string();
+                let num_reps = id_str.len()/div;
+                let actual_reps = id_str.matches(&min_sxn).count();
+                // println!(" Min sxn: {:?}, num_reps: {}, actual_reps: {}", min_sxn, num_reps, actual_reps);
+                if (num_reps > 1) & (num_reps == actual_reps) {
+                    num_invalid += 1;
+                    invalid_sum += id;
+                    // debug
+                    // println!("invalid {id}");
+                    // don't count the same one multiple times!
+                    break;
+                }
+            }
+        }
+    }
+
+    println!("There are now {} invalid ids, for a sum of {}", num_invalid, invalid_sum);
+    // 43872163557
 
 
     Ok(())
