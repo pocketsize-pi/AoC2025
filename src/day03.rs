b055e566fd40207c9b41cbc20ce122aae7d9f190
pocketsize-pi <35309820+pocketsize-pi@users.noreply.github.com>
2025-12-04 14:34:16 +0000

    Day 3 part 2
    I get stuck even before the calculations. So I'm going to park this now and look at day 4 first.

--- src/day03.rs
+++ src/day03.rs
@@ -1,5 +1,7 @@
 use std::cmp::max;
-use combinatorial::Combinations;
+// use combinatorial::Combinations;
+// use itertools::Itertools;
+use combination::*;
 use aoc_2025::*;
 
 pub fn day03(input_type: InputType, manual_name: &str) -> Result<(), Box<dyn std::error::Error>> {
@@ -36,22 +38,57 @@ pub fn day03(input_type: InputType, manual_name: &str) -> Result<(), Box<dyn std
     // 17412
 
     // cool, cool, cool, 12...
-    // let's bring in combinatorial help!
-    //ombinations::of_size(vec!['a', 'b', 'c'], 2);
+    // let's bring in combinatorial help
+    let mut tried = Vec::new();
+
     let mut big_max_jolt = 0;
+    let mut n=0;
     for battery in batteries {
         let mut local_max=0;
+        n+=1;
+        println!("Battery {}",n);
         // println!("{:?}", battery);
-        let choose_12 = Combinations::of_size(0..battery.len(),12);
+        // let choose_12 = Combinations::of_size(0..battery.len(),12);
+        // // println!("{:?}", battery);
+        // // println!("{:?}", choose_12);
+        // for combo_id in choose_12 {
+        //     // println!("{:?}", combo_id);
+        //     // smush the numbers together
+        //     let combo_string = combo_id.iter().filter_map(|i| Some(battery[*i].to_string())).collect::<String>();
+        //     // println!("{:?}", combo_string);
+        //     local_max = max(local_max, str_to_u64(&*combo_string));
+        // }
+
         // println!("{:?}", battery);
-        // println!("{:?}", choose_12);
-        for combo_id in choose_12 {
-            // println!("{:?}", combo_id);
-            // smush the numbers together
-            let combo_string = combo_id.iter().filter_map(|i| Some(battery[*i].to_string())).collect::<String>();
-            // println!("{:?}", combo_string);
-            local_max = max(local_max, str_to_u64(&*combo_string));
+        // I couldn't get combinatorial to work
+        // oh! generate combination is what is keeping our program stuck!
+        let mut choose_12: Vec<Vec<u32>> = combine::from_vec_at(&battery, 12);
+        // let choose_12: Vec<Vec<u32>> = battery.into_iter().combinations(12).collect();
+        // remove duplicates, there must be many
+        // let small_choose_12 = choose_12.into_iter().unique();
+        // println!("orig {:?}", choose_12);
+        choose_12.sort();
+        // println!("sort {:?}", choose_12);
+        choose_12.dedup();
+        // println!("dedup {:?}", choose_12);
+        println!(" ({} combinations)", choose_12.len());
+
+
+        // println!("12 {:?}", choose_12);
+        for combo in choose_12 {
+            if !tried.contains(&combo) {
+                // println!("{:?}", combo_id);
+                // smush the numbers together
+                let combo_string = combo.iter().map(|n| n.to_string()).collect::<String>();
+                //.iter().filter_map(|i| Some(battery[*i].to_string())).collect::<String>();
+                // println!("{:?}", combo_string);
+                local_max = max(local_max, str_to_u64(&*combo_string));
+                tried.push(combo);
+            }
+
         }
+
+
         big_max_jolt += local_max;
 
     }
