5f8fdf448a1d379e857f2b00367da63bb7e241e2
pocketsize-pi <35309820+pocketsize-pi@users.noreply.github.com>
2025-12-10 10:07:39 +0000

    Day 05 part 2 working
    The algorithm was right, I had some issues and some missed cases

--- src/day05.rs
+++ src/day05.rs
@@ -53,18 +53,27 @@ pub fn day05(input_type: InputType, manual_name: &str) -> Result<(), Box<dyn std
         // let output : Option<(u64,u64)>;
         if (next_range.0..next_range.1+1).contains(&this_range.0) &
             (next_range.0..next_range.1+1).contains(&this_range.1) {
+            // this fully contained by next
             None
+        }
+        else if (this_range.0..this_range.1+1).contains(&next_range.0) &
+            (this_range.0..this_range.1+1).contains(&next_range.1) {
+            // next fully contained by this (start with same number!)
+            Some((this_range, true))
         }
         else if (this_range.0..this_range.1+1).contains(&next_range.0) &
             (next_range.0..next_range.1+1).contains(&this_range.1) {
             // starts in this and ends in next
+            // print!("starts in this and ends in next:");
             Some(((this_range.0,next_range.1),true))
         }
         else if (next_range.0..next_range.1+1).contains(&this_range.0) &
             (this_range.0..this_range.1+1).contains(&next_range.1) {
             // starts in next and ends in this
-            Some(((this_range.0,next_range.1),true))
+            // print!("xxxxxxstarts in this and ends in next:");
+            Some(((this_range.0, next_range.1), true))
         }
+            //MISSING IF THIS CONTAINS NEXT!
         else {
             // nothing to merge, push;
             Some((this_range, false))
@@ -77,27 +86,29 @@ pub fn day05(input_type: InputType, manual_name: &str) -> Result<(), Box<dyn std
     loop {
 
         let mut new_fresh = Vec::new();
-        let mut skip_this = false;
-        // println!("{:?}", possible_fresh);
+        let mut skip_next = false;
+        // println!("---------{:?}", possible_fresh);
         for r in 0..possible_fresh.len()-1 {
 
-            if skip_this  {
-                skip_this = false;
+            if skip_next {
+                skip_next = false;
                 continue;
             }
             // ranges is sorted, so we just need to build a list that is good and has no overlap
-            let this_range = fresh_ranges[r];
+            let this_range = possible_fresh[r];
             // process last point
-            let next_range = fresh_ranges[r+1];
+            let next_range = possible_fresh[r+1];
 
+            // println!("{:?}, {:?}", this_range, next_range);
             let reduction = compare_two_pairs(this_range, next_range);
 
             match reduction {
                 None => {}
                 Some((new_range,skip)) => {
                     new_fresh.push(new_range);
+                    // println!("{:?}, skip: {skip}", new_range);
                     if skip {
-                        skip_this = true;
+                        skip_next = true;
                     }
                 }
             }
@@ -108,7 +119,9 @@ pub fn day05(input_type: InputType, manual_name: &str) -> Result<(), Box<dyn std
         let reduction = compare_two_pairs(*new_fresh.last().unwrap(), *possible_fresh.last().unwrap());
 
         match reduction {
-            None => {*new_fresh.last_mut().unwrap() = *possible_fresh.last().unwrap() }
+            None => {
+                // println!("replacing last fresh!");
+                *new_fresh.last_mut().unwrap() = *possible_fresh.last().unwrap() }
             Some((new_range, skip)) => {
                 if skip {
                     new_fresh.push(new_range);
@@ -126,6 +139,7 @@ pub fn day05(input_type: InputType, manual_name: &str) -> Result<(), Box<dyn std
             break;
         }
         possible_fresh = new_fresh.clone();
+        possible_fresh.sort(); // just in case
 
         // i += 1;
         // if i >= 10 {
@@ -134,6 +148,8 @@ pub fn day05(input_type: InputType, manual_name: &str) -> Result<(), Box<dyn std
 
     }
 
+    // println!("{:?}", possible_fresh);
+
     total_fresh = 0;
     for range in possible_fresh {
         total_fresh += (range.0..range.1+1).count();
@@ -141,6 +157,8 @@ pub fn day05(input_type: InputType, manual_name: &str) -> Result<(), Box<dyn std
 
     println!("The total possible fresh ingredients is {}", total_fresh);
     // 11753977975152 too low
+    // 375947484460456 too high
+    // 365804144481581 hurrah!
 
 
 
